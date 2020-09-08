8d8cd04c5c3aeaf0190fc60d3fecf0057f4c8438
Nuno Das Neves <nunodasneves@linux.microsoft.com>
2020-09-08 11:41:44 -0700

    drivers/hv: map and unmap guest memory
    Introduce ioctls for mapping and unmapping regions of guest memory.
    
    Uses a table of memory 'slots' similar to KVM, but the slot
    number is not visible to userspace.
    
    For now, this simple implementation requires each new mapping to be
    disjoint - the underlying hypercalls have no such restriction, and
    implicitly overwrite any mappings on the pages in the specified regions.
    
    Co-developed-by: Lillian Grassin-Drake <ligrassi@microsoft.com>
    Signed-off-by: Lillian Grassin-Drake <ligrassi@microsoft.com>
    Signed-off-by: Nuno Das Neves <nunodasneves@linux.microsoft.com>

--- include/uapi/linux/mshv.h
+++ include/uapi/linux/mshv.h
@@ -19,10 +19,25 @@ struct mshv_create_partition {
 	struct hv_partition_creation_properties partition_creation_properties;
 };
 
+/*
+ * Mappings can't overlap in GPA space or userspace
+ * To unmap, these fields must match an existing mapping
+ */
+struct mshv_user_mem_region {
+	__u64 size;		/* bytes */
+	__u64 guest_pfn;
+	__u64 userspace_addr;	/* start of the userspace allocated memory */
+	__u32 flags;		/* ignored on unmap */
+};
+
 #define MSHV_IOCTL 0xB8
 
 /* mshv device */
 #define MSHV_CHECK_EXTENSION    _IOW(MSHV_IOCTL, 0x00, __u32)
 #define MSHV_CREATE_PARTITION	_IOW(MSHV_IOCTL, 0x01, struct mshv_create_partition)
+
+/* partition device */
+#define MSHV_MAP_GUEST_MEMORY	_IOW(MSHV_IOCTL, 0x02, struct mshv_user_mem_region)
+#define MSHV_UNMAP_GUEST_MEMORY	_IOW(MSHV_IOCTL, 0x03, struct mshv_user_mem_region)
 
 #endif
