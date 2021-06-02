f9366dce281ddec46c664e44cbce8564af7dfa8a
Wei Liu <wei.liu@kernel.org>
2021-06-02 17:28:47 +0000

    x86/hyperv: export hv_build_pci_dev_id
    Signed-off-by: Wei Liu <wei.liu@kernel.org>

--- arch/x86/include/asm/mshyperv.h
+++ arch/x86/include/asm/mshyperv.h
@@ -195,6 +195,7 @@ struct irq_domain *hv_create_pci_msi_domain(void);
 int hv_map_ioapic_interrupt(int ioapic_id, bool level, int vcpu, int vector,
 		struct hv_interrupt_entry *entry);
 int hv_unmap_ioapic_interrupt(int ioapic_id, struct hv_interrupt_entry *entry);
+union hv_device_id hv_build_pci_dev_id(struct pci_dev *dev);
 
 #else /* CONFIG_HYPERV */
 static inline void hyperv_init(void) {}
