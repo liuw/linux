7b175afae3e9191c5b058ea54aa445c79a40052b
Wei Liu <wei.liu@kernel.org>
2021-07-01 14:11:57 +0000

    intel/vt-d: export intel_iommu_get_resv_regions
    When Microsoft Hypervisor runs on Intel platforms it needs to know the
    reserved regions to program devices correctly. There is no reason to
    duplicate intel_iommu_get_resv_regions. Export it.
    
    Signed-off-by: Wei Liu <wei.liu@kernel.org>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -5191,8 +5191,8 @@ static void intel_iommu_probe_finalize(struct device *dev)
 	iommu_setup_dma_ops(dev, 0, U64_MAX);
 }
 
-static void intel_iommu_get_resv_regions(struct device *device,
-					 struct list_head *head)
+void intel_iommu_get_resv_regions(struct device *device,
+				 struct list_head *head)
 {
 	int prot = DMA_PTE_READ | DMA_PTE_WRITE;
 	struct iommu_resv_region *reg;
@@ -5247,6 +5247,7 @@ static void intel_iommu_get_resv_regions(struct device *device,
 		return;
 	list_add_tail(&reg->list, head);
 }
+EXPORT_SYMBOL_GPL(intel_iommu_get_resv_regions);
 
 int intel_iommu_enable_pasid(struct intel_iommu *iommu, struct device *dev)
 {
--- include/linux/intel-iommu.h
+++ include/linux/intel-iommu.h
@@ -816,6 +816,8 @@ extern int iommu_calculate_max_sagaw(struct intel_iommu *iommu);
 extern int dmar_disabled;
 extern int intel_iommu_enabled;
 extern int intel_iommu_gfx_mapped;
+extern void intel_iommu_get_resv_regions(struct device *device,
+				 struct list_head *head);
 #else
 static inline int iommu_calculate_agaw(struct intel_iommu *iommu)
 {
@@ -827,6 +829,8 @@ static inline int iommu_calculate_max_sagaw(struct intel_iommu *iommu)
 }
 #define dmar_disabled	(1)
 #define intel_iommu_enabled (0)
+static inline void intel_iommu_get_resv_regions(struct device *device,
+				 struct list_head *head) {}
 #endif
 
 static inline const char *decode_prq_descriptor(char *str, size_t size,
